5d82aa4ee783ff81ef7c77e8acbde3d80b7dbe1f
QAston <qaston@gmail.com>
2016-01-20 22:55:55 +0100

    Experimental implementation mapping, taking and into.

--- source/transduced.d
+++ source/transduced.d
@@ -28,6 +28,7 @@ import std.range;
 import std.functional;
 import std.range.primitives;
 import std.array;
+import std.traits;
 
 auto foldr(fun, S, R)(S seed, R range) {
 // reduce starting from collection end, using tail recursion
@@ -57,16 +58,50 @@ auto filterl(alias f, S, R) (ref S s, R r) {
 	return reduce!(filterReducing!(f, S, typeof(r.front)))(s, r);
 }
 
-// wrap a struct using typecons to mark it Reduced!()
-
-interface RuntimeTransducer(Wrapped, S, T) {
-	typeof(Wrapped.initialize()) initialize();
-	typeof(Wrapped.completed(S.init)) completed(S);
-	typeof(Wrapped.reducing(S.init, T.init)) reducing(S, T);
+mixin template ReducingBase(Wrapped) {
+	Wrapped next;
+	final bool isReduced() @property {
+		return next.isReduced();
+	}
+	final void markReduced() {
+		next.markReduced();
+	}
+	auto completed(ReturnType!(Wrapped.completed) seed) {
+		return next.completed(seed);
+	}
 }
 
-struct Transducer(Wrapped, S, T) {
+auto catting(){
+	static struct Catting(Wrapped) {
+	}
+}
 
+
+// function returning a transducer
+// transducer holds the info on what to do, is a runtime object in clojure
+// does not know anything about the process
+// can be shared
+auto mapping(alias f)() {
+	// transducers create an object which apply the given job description to given step function
+	// the created objs are used privately
+	// type specialization cannot be done at runtime, so polymorphism needed
+	// or not, if transducing contexts are just parametrized with type, but then it'd generate code for int params:(
+
+	static struct Mapping { // transducer
+		auto wrap(Wrapped)(Wrapped next) { //next is a stackof reducing Structs, at the bottom of which is the transducing process
+			static struct Reducing { // reducing "function", no arity-0 equivalent for D because the pattern is not popular in D
+				mixin ReducingBase!(Wrapped);
+				this(Wrapped next) {
+					this.next = next;
+				}
+				auto reducing(T) (ReturnType!(Wrapped.completed) seed, T elem) {
+					return next.reducing(seed, f(elem));
+				}
+			}
+			return Reducing(next);
+		}
+	}
+	return Mapping();
 }
 
 //if a transducer gets a reduced value from nested step call it must never call that step function again with input
@@ -77,32 +112,35 @@ struct Transducer(Wrapped, S, T) {
 // reduced value could be implemented by some kind of variant type
 // or maybe by a transducer method instead?
 
-auto taking(int howMany) {
-	static struct Taking(Wrapped) {
-		this(int howMany) {
+auto taking(size_t howMany) {
+	static struct Taking {
+		private size_t howMany;
+		this(size_t howMany) {
 			this.howMany = howMany;
 		}
-		Wrapped next;
-		int howMany;
-		auto initialize(){
-			next.initialize();
-		}
-		auto completed(S seed) {
-			return next.completed(seed);
-		}
-		auto reducing(S, T) (S seed, T elem) {
-			auto result = next.reducing(seed, f(elem));
-			if (--howMany < 0)
-				return result; // reduced
-			return result;
+		auto wrap(Wrapped)(Wrapped next) {
+			static struct Reducing {
+				mixin ReducingBase!(Wrapped);
+				private size_t howMany;
+				this(Wrapped next, size_t howMany) {
+					this.next = next;
+					this.howMany = howMany;
+				}
+				auto reducing(T) (ReturnType!(Wrapped.completed) seed, T elem) {
+					if (--howMany == howMany.max) {
+						this.markReduced();
+						return seed;
+					}
+					auto result = next.reducing(seed, elem);
+					return result;
+				}
+			}
+			return Reducing(next, howMany);
 		}
 	}
+	return Taking(howMany);
 }
 
-auto catting(){
-	static struct Catting(Wrapped) {
-	}
-}
 // TransducibleContext
 
 // transduce
@@ -120,42 +158,37 @@ auto transduce(alias reducingFn, R, Transducer, S)(R r, S s, Transducer t) {
 	return transducerStack.completed(returnValue);
 }
 
+// populates output range with input range processed by a transducer
+auto into(Out, Transducer, R)(Out to, Transducer t, R from) {
+	static struct IntoProcess {
+		private bool reduced;
+		final bool isReduced() @property {
+			return reduced;
+		}
+		final void markReduced() {
+			reduced = true;
+		}
+		Out completed(Out seed) {
+			return seed;
+		}
+		Out reducing(Out seed, ElementType!Out elem) {
+			put(seed, elem);
+			return seed;
+		}
+	}
+	auto transducerStack = t.wrap(IntoProcess());
+	foreach (el; from) {
+		to = transducerStack.reducing(to, el);
+		if(transducerStack.isReduced())
+			break;
+	}
+	return transducerStack.completed(to);
+}
+
 // educe
 // pin collection to transducer, created a sequence on which element reads execute transducer code
 
 
-
-// function returning a transducer
-// transducer holds the info on what to do, is a runtime object in clojure
-// does not know anything about the process
-// can be shared
-auto mapping(alias f)() {
-	// transducers create an object which apply the given job description to given step function
-	// the created objs are used privately
-	// type specialization cannot be done at runtime, so polymorphism needed
-	// or not, if transducing contexts are just parametrized with type, but then it'd generate code for int params:(
-
-	static struct Mapping { // transducer
-		auto wrap(Wrapped)(Wrapped next) { //next is a stackof reducing Structs, at the bottom of which is the transducing process
-			static struct Reducing { // reducing "function"
-				Wrapped next;
-				// this is optional
-				// transducers never implement this
-				auto initialize(){
-					return next.initialize();
-				}
-				auto completed(S seed) {
-					return next.completed(seed);
-				}
-				auto reducing(S, T) (S seed, T elem) {
-					return next.reducing(seed, f(elem));
-				}
-			}
-			return Reducing();
-		}
-	}
-	return Mapping();
-}
 
 
 int minus(int i) {
@@ -173,4 +206,18 @@ unittest {
 	int[] ar = new int[](4);
 	mapl!(minus)(ar, [1, 2, 3, 4]);
 	assert( ar == [-1, -2, -3, -4]);
+}
+
+unittest {
+	int[] ar = new int[](4);
+	auto transducer = mapping!minus;
+	into(ar,transducer, [1, 2, 3, 4]);
+	assert( ar == [-1, -2, -3, -4]);
+}
+
+unittest {
+	int[] ar = new int[](4);
+	auto transducer = taking(2);
+	into(ar,transducer, [1, 2, 3, 4]);
+	assert( ar == [1, 2, 0, 0]);
 }
